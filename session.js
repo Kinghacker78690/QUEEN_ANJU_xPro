321e1d4b6eb95411ebc5f1d5376ee4c7cc66e28d
Kinghacker78690 <kinghackerkinghacker786@gmail.com>
2025-02-25 21:38:34 -0800

    Update session.js

--- session.js
+++ session.js
@@ -40,6 +40,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your session id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~mcc10KSI#ihc7S9VTzfIPP01GprnI_1B6qyvXyyJZVjSaPaaNiuA",
 PORT: process.env.PORT || "8000"
 };
